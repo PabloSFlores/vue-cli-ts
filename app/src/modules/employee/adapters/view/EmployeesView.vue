6865109be2aa1c396155dcf629dbe90305c4d182
PabloSFlores <20213tn013@utez.edu.mx>
2023-05-16 00:11:36 -0600

    Excel modal

--- app/src/modules/employee/adapters/view/EmployeesView.vue
+++ app/src/modules/employee/adapters/view/EmployeesView.vue
@@ -4,17 +4,33 @@
         <div class="mx-5 mt-5">
             <div class="d-flex align-items-center">
                 <h1>Gestión de Empleados</h1>
-                <button v-b-modal.modal-1 class="btn btn-success ml-auto">Añadir</button>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <section class="text-start">
+                        <button class="btn btn-success m-1">Excel</button>
+                        <button class="btn btn-danger m-1">PDF</button>
+                    </section>
+                </div>
+                <div class="col-md-6">
+                    <section class="text-end">
+                        <button class="btn btn-success m-1" v-b-modal.modal-3>Subida con Excel</button>
+                        <button class="btn btn-primary m-1" v-b-modal.modal-2>agregar</button>
+                    </section>
+                </div>
             </div>
             <hr>
-            <b-table striped hover :items="employees" :fields="fields" >
+            <b-table striped hover :fields="fields" :items="employees" :per-page="7" :current-page="currentPageMain">
                 <template #cell(actions)="data">
-                    <button class="btn btn-primary" @click="findEmployee(data.item.id)" v-b-modal.modal-2>info</button>
+                    <button class="btn btn-info" @click="findEmployee(data.item.id)" v-b-modal.modal-1>Actualizar</button>
                 </template>
             </b-table>
+            <b-pagination v-model="currentPageMain" :total-rows="employees.length" :per-page="7"
+                aria-controls="my-table"></b-pagination>
         </div>
-        <AddModal @findAll="findAll"/>
-        <UpdateModal :employeeSelected="employee" @findAll="findAll"/>
+        <AddModal @findAll="findAll" />
+        <ImportExcelModal @findAll="findAll" />
+        <UpdateModal :employeeSelected="employee" @findAll="findAll" />
     </div>
 </template>
 
@@ -23,25 +39,27 @@ import Vue from 'vue';
 import Topnav from '../../../../components/Top-Nav.vue';
 import AddModal from './AddEmployee.modal.vue';
 import UpdateModal from './UpdateEmployee.modal.vue';
+import ImportExcelModal from './ImportExcel.modal.vue';
 import { EmployeeController } from '../employees.controller';
 import { Employee } from '../../entities/employee';
 
 export default Vue.extend({
     name: 'EmployeesView',
     components: {
-        Topnav, AddModal, UpdateModal
+        Topnav, AddModal, UpdateModal, ImportExcelModal
     },
     data() {
         return {
             fields: [
-                {key: 'name', label: 'Nombre', sortable: true,},
-                {key: 'surname', label: 'Primer apellido', sortable: true,},
-                {key: 'lastname', label: 'Segundo apellido', sortable: true,},
-                {key: 'email', label: 'Correo', sortable: true,},
-                {key: 'actions', label: 'Acciones'}
+                { key: 'name', label: 'Nombre', sortable: true, },
+                { key: 'surname', label: 'Primer apellido', sortable: true, },
+                { key: 'lastname', label: 'Segundo apellido', sortable: true, },
+                { key: 'email', label: 'Correo', sortable: true, },
+                { key: 'actions', label: 'Acciones' }
             ],
             employees: [] as Employee[],
-            employee: {} as Employee
+            employee: {} as Employee,
+            currentPageMain: 1
         }
     },
     methods: {
@@ -56,8 +74,8 @@ export default Vue.extend({
             this.employee = response.entity!;
         }
     },
-    mounted () {
+    mounted() {
         this.findAll();
-    } 
+    }
 });
 </script>
