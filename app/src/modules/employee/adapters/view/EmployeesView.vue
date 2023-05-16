89ef13498d12658136acb32923b3a420dd9a9e9e
PabloSFlores <20213tn013@utez.edu.mx>
2023-05-16 15:24:38 -0600

    Generar excel

--- app/src/modules/employee/adapters/view/EmployeesView.vue
+++ app/src/modules/employee/adapters/view/EmployeesView.vue
@@ -13,7 +13,7 @@
                         <div class="row">
                             <div class="col-md-3">
                                 <section class="text-center">
-                                    <button class="btn btn-success m-1">Excel</button>
+                                    <button class="btn btn-success m-1" @click="generateExcel">Excel</button>
                                 </section>
                             </div>
                             <div class="col-md-3">
@@ -28,7 +28,7 @@
                             </div>
                             <div class="col-md-3">
                                 <section class="text-center">
-                                    <button class="btn btn-primary m-1" v-b-modal.modal-1>agregar</button>
+                                    <button class="btn btn-primary m-1" v-b-modal.modal-1>Agregar</button>
                                 </section>
                             </div>
                         </div>
@@ -38,7 +38,7 @@
                                     :current-page="currentPageMain">
                                     <template #cell(actions)="data">
                                         <button class="btn btn-info" @click="findEmployee(data.item.id)"
-                                            v-b-modal.modal-2>actualizar</button>
+                                            v-b-modal.modal-2>Actualizar</button>
                                         <button class="btn btn-danger mx-2" @click="findEmployee(data.item.id)"
                                             v-b-modal.modal-4>Eliminar</button>
                                     </template>
@@ -70,6 +70,7 @@ import { Employee } from '../../entities/employee';
 import pdfMake from 'pdfmake/build/pdfmake.js';
 import pdfFonts from 'pdfmake/build/vfs_fonts.js';
 import { SaveEmployeeDto } from '../dto/save-employee';
+import * as XLSX from 'xlsx';
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 export default Vue.extend({
@@ -110,7 +111,7 @@ export default Vue.extend({
                         style: "tableExample",
                         table: {
                             widths: [
-                                '*', '*','*','*'
+                                '*', '*', '*', '*'
                             ],
                             body: [["Nombre", "Apellido Materno", "Apellido Paterno", "Correo"]],
                         },
@@ -132,16 +133,41 @@ export default Vue.extend({
                     email: element.email
                 }
                 this.document.content[0].table.body.push([
-                    { text: `${employee.name}`},
-                    { text: `${employee.surname}`},
-                    { text: `${employee.lastname}`},
-                    { text: `${employee.email}`},
+                    { text: `${employee.name}` },
+                    { text: `${employee.surname}` },
+                    { text: `${employee.lastname}` },
+                    { text: `${employee.email}` },
                 ]);
             }
             console.table(this.document)
-            
-            pdfMake.createPdf(this.document).download('Reporte')
+
+            pdfMake.createPdf(this.document).download(`Reporte_${(new Date()).toString().replace(' ', '-')}`)
             this.document = null
+        },
+        generateExcel() {
+            const aoa = [] as any
+            this.employees.map((employee) => {
+                aoa.push([`${employee.name}`, `${employee.surname}`, `${employee.lastname}`, `${employee.email}`])
+            })
+            const worksheet = XLSX.utils.aoa_to_sheet(aoa);
+            XLSX.utils.sheet_add_aoa(worksheet, [["Nombre", "Apellido 1", "Apellido 2", "Correo"]], { origin: "A1" });
+            const workbook = XLSX.utils.book_new();
+            XLSX.utils.book_append_sheet(workbook, worksheet, "Libro1");
+            const excelBuffer = XLSX.write(workbook, {
+                type: "buffer",
+                bookType: "xlsx",
+            });
+            const blob = new Blob([excelBuffer], {
+                type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            });
+
+            const url = URL.createObjectURL(blob);
+            const link = document.createElement("a");
+            link.href = url;
+            link.download = `Reporte_${(new Date()).toString().replace(' ','-')}.xlsx`;
+            link.click();
+
+            URL.revokeObjectURL(url)
         }
     },
     mounted() {
